267675ca404c55a141400aba3d9cadcedd1e3751
louis <louis.snoriguzzi@epitech.eu>
2021-05-20 21:33:24 +0100

    Get rubric by name and get all rubrics + unit tests

--- src/Controller.java
+++ src/Controller.java
@@ -9,4 +9,15 @@ public class Controller {
     public void addRubric(String name) {
         this._rubrics.add(new Rubric(name));
     }
+
+    public Rubric getRubricByName(String name) throws Exception {
+        for (int i = 0; i <= _rubrics.size(); i = i + 1) {
+            if (name.equals(_rubrics.get(i)._name)) {
+                return (_rubrics.get(i));
+            }
+        }
+        throw new Exception("Rubric not found");
+    }
+
+    public List<Rubric> getAllRubrics() { return this._rubrics; }
 }
--- src/ControllerTest.java
+++ src/ControllerTest.java
@@ -1,12 +1,33 @@
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ControllerTest {
     Controller controller = new Controller();
+
     @org.junit.jupiter.api.Test
     void addRubric() {
         assertEquals(0, controller._rubrics.size());
-        controller.addRubric("Rubric");
-        assertEquals(1, controller._rubrics.size());
-        assertEquals("Rubric", controller._rubrics.get(0)._name);
+        controller.addRubric("Rubric1");
+        controller.addRubric("Rubric2");
+        assertEquals(2, controller._rubrics.size());
+        assertEquals("Rubric1", controller._rubrics.get(0)._name);
+    }
+
+    @org.junit.jupiter.api.Test
+    void getRubricByName() throws Exception {
+        controller.addRubric("Rubric1");
+        controller.addRubric("Rubric2");
+        Rubric rubric = controller.getRubricByName("Rubric1");
+        assertEquals("Rubric1", rubric._name);
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAllRubrics() {
+        controller.addRubric("Rubric1");
+        controller.addRubric("Rubric2");
+        List<Rubric> rubrics = controller.getAllRubrics();
+        assertEquals("Rubric1", controller._rubrics.get(0)._name);
+        assertEquals("Rubric2", controller._rubrics.get(1)._name);
     }
 }
